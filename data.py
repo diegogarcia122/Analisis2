9fe15ca4e199dcdd3b4d4478af32fb6dea9c8e0b
diegogarcia122 <111999491+diegogarcia122@users.noreply.github.com>
2024-09-27 12:11:38 -0600

    last

--- data.py
+++ data.py
@@ -7,25 +7,24 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 df = pd.read_csv('Reviews.csv')
 
-#print(df.info())
+print(df.info())
 df['Summary'] = df['Summary'].astype(str)
 pretext = df['Summary']
 score = df['Score']
-#plt = pl.bar(df, x='Score')
-#plt.show()
+plt = pl.bar(df, x='Score')
+plt.show()
 
 text = " ".join(i for i in pretext)
 stopwords = set(STOPWORDS)
 wordcloud = WordCloud(stopwords=stopwords, background_color="black").generate(text)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
-#plt.show()
+plt.show()
 
 #Sentiment
 df = df[df['Score'] != 3]
 df['sentiment'] = df['Score'].apply(lambda x: 1 if x > 3 else -1)
-
-#print(df.head())
+print(df.head())
 
 #positive
 filtered_df = df[(df['sentiment'] == 1)]
@@ -34,7 +33,7 @@ stopwords = set(STOPWORDS)
 wordcloud = WordCloud(stopwords=stopwords, background_color="black").generate(text)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
-#plt.show()
+plt.show()
 
 #negative
 filtered_df = df[(df['sentiment'] == -1)]
@@ -43,31 +42,28 @@ stopwords = set(STOPWORDS)
 wordcloud = WordCloud(stopwords=stopwords, background_color="black").generate(text)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
-#plt.show()
+plt.show()
 
-#plt = pl.bar(df, x='sentiment')
-#plt.show()
+plt = pl.bar(df, x='sentiment')
+plt.show()
 
 pattern = r'[^\w\s]'
 df['Summary'] = df['Summary'].replace(pattern, '', regex=True)
 
 division = df[['Summary','sentiment']]
-#print(division.head())
+print(division.head())
+
+
+print(len(division))
+split_index = int(len(division) * 0.8)
+df_80 = df.iloc[:split_index]
+df_20 = df.iloc[split_index:]
+
+print("Training DataFrame (80%):")
+print(len(df_80))
+print("\nTesting DataFrame (20%):")
+print(len(df_20))
 
 vectorizer = CountVectorizer()
-vectorizer.fit(df[['Summary','sentiment']])
 
-print("Vocabulary: ", vectorizer.vocabulary_)
-vector = vectorizer.transform(df[['Summary','sentiment']])
- 
-print("Encoded Document is:")
-print(vector.toarray())
-
-
-
-
-
-
-
-
-
+bag_of_words = vectorizer.fit_transform(df_80)
